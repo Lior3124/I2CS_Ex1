680fd872df41c90af779054241fd21823c1154c4
Lior Lyubarsky <lior.lyubarsky@gmail.com>
2025-11-24 22:42:13 +0200

    sameValue complete + test passed

--- Ex1.java
+++ Ex1.java
@@ -78,7 +78,7 @@ public class Ex1 {
         /** add you code below*/
 
         int max = Math.max(p1.length, p2.length);
-        max = max+1;
+        max = max;
 
         double check= Math.random()*(-10);
         while(max != 0) {
@@ -122,11 +122,21 @@ public class Ex1 {
 	 * @return an x value (x1<=x<=x2) for which |p1(x) - p2(x)| < eps.
 	 */
 	public static double sameValue(double[] p1, double[] p2, double x1, double x2, double eps) {
-		double ans = x1;
-        /** add you code below
-
-         /////////////////// */
-		return ans;
+		//double ans = x1;
+        /** add you code below*/
+        double x = (x1+x2)/2;
+        double fx1 = f(p1,x);
+        double fx2 = f(p2,x);
+        if(Math.abs(fx1 -fx2)<eps) {
+            return x;
+        }
+        if((fx1 -fx2)*(f(p1,x1)-f(p2,x1))>0){
+            return sameValue(p1, p2, x, x2, eps);
+        }else{
+            return sameValue(p1, p2, x1, x, eps);
+        }
+         ///////////////////
+		//return ans;
 	}
 	/**
 	 * Given a polynomial function (p), a range [x1,x2] and an integer with the number (n) of sample points.
